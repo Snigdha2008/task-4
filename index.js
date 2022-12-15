a1ee6d74d660d6483513667b6562f423a0815837
Snigdha2008 <118926384+Snigdha2008@users.noreply.github.com>
2022-12-15 08:08:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const companies = [
+    { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+];
+companies.forEach(myfunction)
+function myfunction(value) {
+    console.log(value.name);
+}
+console.log("companies after 1987");
+for (var x = 0; x < companies.length; x++) {
+    if (companies[x].start > 1987) {
+        console.log(companies[x].name);
+    }
+}
+
